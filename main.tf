4d558b15ca262382b7bab932b2e1125d22dd2752
San <46291282+Sankeerth-Chillamcharla@users.noreply.github.com>
2024-02-29 12:30:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,6 @@ provider "aws" {
 region = "us-east-1"
 }
 resource "aws_instance" "test-server" {
-ami = "us-east-1"
-instance_type = "t2.micro" }
+  ami = "us-east-1"
+  instance_type = "t2.micro" 
+}
